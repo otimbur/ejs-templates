e5cb28a11efcc4fff5a96eb9ecdb3167d70b5bd4
Olga <103147467+otimbur@users.noreply.github.com>
2022-08-11 23:13:48 +0000

    notejs

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs')
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) =>{ 
+  let title = 'Our Home Page'
+  let heading = 'My Website '
+  res.render('pages/index',{
+    'title':title,
+    'heading':heading
+  })
+})
+app.get('/about', (req, res) =>{ 
+  let title = 'Our About Page'
+  let heading = 'My Website '
+  res.render('pages/about',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
